218f010cea2401f4e9e28e335f4664fc63d05f0d
jefferson-s <jewave@hotmail.com>
2021-08-16 22:09:57 -0300

    primeiro commit do javascript

---
+++ main.js
@@ -0,0 +1,52 @@
+
+
+var d = new Date();
+    alert(d.getTimezoneOffset());
+
+/*
+var count;
+for (count = 0 ; count <= 5; count++)
+{
+    alert (count);
+}
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log (count);
+    alert (count);
+    count++;
+}*/
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade > 18){
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+}
+*/
+//var fruta = [{ nome:"maça", cor: "vermelha"}, { nome:"uva", cor: "roxa"}]
+//console.log (fruta.nome);
+//alert (fruta [1].nome);
+
+
+//var fruta = { nome:"maça", cor: "vermelha"}
+//console.log (fruta.nome);
+//alert (fruta.cor);
+//var lista = ["maça","pêra","laranja"];
+//lista.push ("uva");
+//lista.pop ();
+//console.log (lista.reverse);
+//alert (lista[1]);
+//console.log(lista.toString());
+//console.log (lista.join("-"))
+
+//var nome = "Jefferson Kadaj";
+//var idade = 36;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo" + nome);
+//console.log (nome);
+//console.log (idade + idade2);
+//console.log (frase.replace ("Japão", "Brasil"));
